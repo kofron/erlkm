8a8ce9babcc598c4b58aadd878e53a99d9ccc5b5
Jared Kofron <jared.kofron@gmail.com>
2013-12-12 15:23:12 -0500

    autogenerated modules by regar

---
+++ src/erlkm_app.erl
@@ -0,0 +1,16 @@
+-module(erlkm_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    erlkm_sup:start_link().
+
+stop(_State) ->
+    ok.
